cc9ae8c3d9ee600ba599268f1ee57a331e035b0e
desaisoumya <desaisoumya16@gmail.com>
2024-09-11 13:32:04 -0400

    lint errors fixed

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -68,10 +68,10 @@ def registration(request):
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
-        user = User.objects.create_user(username=username, 
+        user = User.objects.create_user(username=username,
                                         first_name=first_name,
-                                        last_name=last_name, 
-                                        password=password, 
+                                        last_name=last_name,
+                                        password=password,
                                         email=email)
         # Login the user and redirect to list page
         login(request, user)
@@ -131,7 +131,7 @@ def add_review(request):
             return JsonResponse({"status": 200})
         except Exception as e:
             print(f"Error: {e}")
-            return JsonResponse({"status": 401, 
+            return JsonResponse({"status": 401,
                                 "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
@@ -145,6 +145,6 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, 
+        cars.append({"CarModel": car_model.name,
                     "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
